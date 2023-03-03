e28802cbfa02d9b0f64cb84781019c29cbe5e362
Islam Hamada <islamsalah@gmail.com>
2023-03-03 23:50:45 +0200

    OTOP2 geneplot

--- crc.R
+++ crc.R
@@ -1,6 +1,13 @@
 # Read in the raw read counts
-rawCounts <- read.delim("/home/islam/Downloads/E-GEOD-50760-raw-counts (1).tsv")
+rawCountstemp <- read.delim("/home/islam/Downloads/E-GEOD-50760-raw-counts (1).tsv")
 head(rawCounts)
+
+
+tmp <- (which(rawCountstemp$Gene.Name != "" ))
+
+write.table(tmp, file='test.tsv')
+
+length(unique(rawCountstemp$Gene.Name))
 
 # Read in the sample mappings
 sampleData <- read.delim("/home/islam/Downloads/E-GEOD-50760-experiment-design (1).tsv")
@@ -43,3 +50,82 @@ library(DESeq2)
 
 # Create the DEseq2DataSet object
 deseq2Data <- DESeqDataSetFromMatrix(countData=rawCounts, colData=sampleData, design= ~ individualID + tissueType)
+
+
+dim(deseq2Data)
+dim(deseq2Data[rowSums(counts(deseq2Data)) > 5, ])
+
+
+# Perform pre-filtering of the data
+deseq2Data <- deseq2Data[rowSums(counts(deseq2Data)) > 5, ]
+
+BiocManager::install("BiocParallel")
+
+library(BiocParallel)
+
+register(MulticoreParam(4))
+
+deseq2Data <- DESeq(deseq2Data, parallel = TRUE)
+
+
+# Extract differential expression results
+# For "tissueType" perform primary vs normal comparison
+deseq2Results <- results(deseq2Data, contrast=c("tissueType", "primary colorectal cancer", "normal-looking surrounding colonic epithelium"))
+
+# View summary of results
+summary(deseq2Results)
+
+
+plotMA(deseq2Results)
+
+
+# Load libraries
+# install.packages(c("ggplot2", "scales", "viridis"))
+library(ggplot2)
+library(scales) # needed for oob parameter
+library(viridis)
+
+# Coerce to a data frame
+deseq2ResDF <- as.data.frame(deseq2Results)
+
+# Examine this data frame
+head(deseq2ResDF)
+
+# Set a boolean column for significance
+deseq2ResDF$significant <- ifelse(deseq2ResDF$padj < .1, "Significant", NA)
+
+# Plot the results similar to DEseq2
+ggplot(deseq2ResDF, aes(baseMean, log2FoldChange, colour=significant)) + geom_point(size=1) + scale_y_continuous(limits=c(-3, 3), oob=squish) + scale_x_log10() + geom_hline(yintercept = 0, colour="tomato1", size=2) + labs(x="mean of normalized counts", y="log fold change") + scale_colour_manual(name="q-value", values=("Significant"="red"), na.value="grey50") + theme_bw()
+
+# Let's add some more detail
+ggplot(deseq2ResDF, aes(baseMean, log2FoldChange, colour=padj)) + geom_point(size=1) + scale_y_continuous(limits=c(-3, 3), oob=squish) + scale_x_log10() + geom_hline(yintercept = 0, colour="darkorchid4", size=1, linetype="longdash") + labs(x="mean of normalized counts", y="log fold change") + scale_colour_viridis(direction=-1, trans='sqrt') + theme_bw() + geom_density_2d(colour="black", size=2)
+
+
+length(which(deseq2ResDF$significant == "Significant"))
+
+
+# Extract counts for the gene otop2
+otop2Counts <- plotCounts(deseq2Data, gene="ENSG00000183034", intgroup=c("tissueType", "individualID"), returnData=TRUE)
+
+# Plot the data using ggplot2
+colourPallette <- c("#7145cd","#bbcfc4","#90de4a","#cd46c1","#77dd8e","#592b79","#d7c847","#6378c9","#619a3c","#d44473","#63cfb6","#dd5d36","#5db2ce","#8d3b28","#b1a4cb","#af8439","#c679c0","#4e703f","#753148","#cac88e","#352b48","#cd8d88","#463d25","#556f73")
+ggplot(otop2Counts, aes(x=tissueType, y=count, colour=individualID, group=individualID)) + geom_point() + geom_line() + theme_bw() + theme(axis.text.x=element_text(angle=15, hjust=1)) + scale_colour_manual(values=colourPallette) + guides(colour=guide_legend(ncol=3)) + ggtitle("OTOP2")
+
+
+deseq2ResDF["ENSG00000183034",]
+rawCounts["ENSG00000183034",]
+normals=row.names(sampleData[sampleData[,"tissueType"]=="normal-looking surrounding colonic epithelium",])
+primaries=row.names(sampleData[sampleData[,"tissueType"]=="primary colorectal cancer",])
+rawCounts["ENSG00000183034",normals]
+rawCounts["ENSG00000183034",primaries]
+
+
+
+
+
+
+
+
+
+
+
